91174a8deaee7b57454f584ba350434b9057b38c
msuero1906 <msuero1906.student@hsfi.us>
2019-10-27 22:00:34 +0000

    today i did two functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+ function firstCharacter(string)
+ {
+     var suffix = string (1, string.length);
+     return suffix + "ay";
+ }
+
+ console.log(firstCharacter(input))
 
 
 
-  
-  
-  
-  
+
+
 });
