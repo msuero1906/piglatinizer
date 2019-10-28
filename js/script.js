d94ae336d63b15cf5ea760fe17bd919a4e50c8ac
msuero1906 <msuero1906.student@hsfi.us>
2019-10-28 22:06:45 +0000

    10/28/19 i actually did something that worked

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
- function firstCharacter(string)
- {
-     var suffix = string (1, string.length);
-     return suffix + "ay";
- }
+ //function firstCharacter(string)
+ $ ("button").click(function(){
+      var word = $ ("input").val();
+      var word= word.substring(0,1);
+  word + "ay";
 
- console.log(firstCharacter(input))
-
-
-
-
-
+    console.log(word);
+    $(".output").text(word);
 });
+});
+//console.log(firstCharacter(".output"));
+ //{
+ //    var suffix = string (1, string.length);
+   //  return suffix + "ay";
+ //}
+
+ //console.log(firstCharacter(input))
+
+
+
+
+
+//});
