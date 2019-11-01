1850671c817fafdb917a23c54e635f09be23d954
msuero1906 <msuero1906.student@hsfi.us>
2019-11-01 01:07:11 +0000

    i finished the project

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
  //function firstCharacter(string)
  $ ("button").click(function(){
-      var word = $ ("input").val();
-      var word= word.substring(0,1);
-  word + "ay";
+      var word = $ (".input").val();
 
-    console.log(word);
-    $(".output").text(word);
+      var suffix = word.substring(0,1);
+
+      var onlyFirstCharacter = word.substring(1,word.length);
+
+       $(".output").text( onlyFirstCharacter + suffix + "ay");
+
+
+
+
+
+
+
+
 });
 });
-//console.log(firstCharacter(".output"));
- //{
- //    var suffix = string (1, string.length);
-   //  return suffix + "ay";
- //}
 
- //console.log(firstCharacter(input))
-
-
-
-
-
-//});
